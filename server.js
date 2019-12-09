8a18f70d0a23edb389b3d6587dd1e32743135ad4
MasterGeniusBR <leo.souza.1996@hotmail.com>
2019-12-09 17:12:28 -0300

    Rotas
    Servidor configurado com rotas definidas manualmente

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const http = require('http');
 
 const server = http.createServer(function(requisicao, resposta)
 {
+    let respostaHtml = ""
+    if(requisicao.url == "/")
+    {
+        respostaHtml = `<p>Servidor em Node do leozika<p>`;
+    }
+    else if(requisicao.url == "/livros")
+    {
+        respostaHtml = `<p>ta na pag de livros<p>`;
+    }
     resposta.end(
-        `<p>Servidor em Node do leozika<p>`
+        respostaHtml
     );
 });
 
