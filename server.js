5cb514c08f3ccbca469e08376e8fcdfd28a07793
MasterGeniusBR <leo.souza.1996@hotmail.com>
2019-12-09 22:15:42 -0300

    Utilizando Express
    Utilizando modulo express para abrir servidor e lidar com rotas

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
-const http = require('http');
+const express = require("express");
+
+const aplicacaoWeb = express();
+
+aplicacaoWeb.listen(3000, function()
+{
+    console.log(`ouvindo na porta 3000`);
+});
+
+aplicacaoWeb.get("/",function(requisicao, resposta)
+{
+    resposta.send(`<h1>Servidor em Node do leozika<h1>`);
+});
+
+aplicacaoWeb.get("/livros",function(requisicao, resposta)
+{
+    resposta.send(`<h2>E aew brow, area dos livros aqui!!<h2>`);
+});
+
+/*const http = require('http');
 
 const server = http.createServer(function(requisicao, resposta)
 {
@@ -16,4 +35,4 @@ const server = http.createServer(function(requisicao, resposta)
     );
 });
 
-server.listen(3000);
+server.listen(3000);*/
